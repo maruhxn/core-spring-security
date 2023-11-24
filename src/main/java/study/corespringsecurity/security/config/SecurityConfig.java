7e2d1cf06e18b513cd1ed9e82b98ea3e591e68c3
maruhxn_ <maruhan1016@gmail.com>
2023-11-24 16:09:56 +0900

    Add Logout

--- src/main/java/study/corespringsecurity/security/config/SecurityConfig.java
+++ src/main/java/study/corespringsecurity/security/config/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -31,7 +29,12 @@ public class SecurityConfig {
                                 .requestMatchers("/messages").hasRole("MANAGER")
                                 .requestMatchers("/config").hasRole("ADMIN")
                                 .anyRequest().authenticated())
-                .formLogin(withDefaults());
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/")
+                                .loginProcessingUrl("/login_proc")
+                                .permitAll());
         return http.build();
     }
 
