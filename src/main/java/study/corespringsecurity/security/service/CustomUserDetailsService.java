55615a087080184e8ede2697ad2afe5ac2b93d43
maruhxn_ <maruhan1016@gmail.com>
2023-11-24 15:53:45 +0900

    Use Filed Injection

--- src/main/java/study/corespringsecurity/security/service/CustomUserDetailsService.java
+++ src/main/java/study/corespringsecurity/security/service/CustomUserDetailsService.java
@@ -1,6 +1,6 @@
 package study.corespringsecurity.security.service;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,10 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
